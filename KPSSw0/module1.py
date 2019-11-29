9c18fa55f32f546c1752ad39297534d5bae62a80
INGPstakra <krawczykstanislaw02@gmail.com>
2019-11-29 09:36:00 +0100

    end

--- KPSSw0/module1.py
+++ KPSSw0/module1.py
@@ -39,7 +39,7 @@ def wymiennikz(y,t,params):
     Tzco,Tpm,Tpco,Tr = y
     mh=3000
     ch=2700
-    Fcob=11.1
+    Fcob=0.011
     kh=12000
     ke=15000
     cb=1000
@@ -54,16 +54,16 @@ def wymiennikz(y,t,params):
     Fzm=params[7]
     Tzm=params[8]
     value=params[10]
-    dTpm=1/(Mm*Cw)*(value*Fzm*cw*ro*(Tzm-Tpm)-kw*(Tpm-Tzco))
-    dTzco=1/(Mco*Cw)*(-Fzco*cw*ro*(Tzco-Tpco)+kw*(Tpm-Tzco))
-    dTpco=1/(mh*ch)*(Fcob*ro*cw*(Tzco-Tpco)-kh*(Tpco-Tr))
-    dTr=1/(mb*cb)*(kh*(Tpco-Tr)-ke*(Tr-8))
+    dTpm=(1/(Mm*Cw))*(value*Fzm*cw*ro*(Tzm-Tpm)-kw*(Tpm-Tzco))
+    dTzco=(1/(Mco*Cw))*(-Fzco*cw*ro*(Tzco-Tpco)+kw*(Tpm-Tzco))
+    dTpco=(1/(mh*ch))*(Fcob*ro*cw*(Tzco-Tpco)-kh*(Tpco-Tr))
+    dTr=(1/(mb*cb))*(kh*(Tpco-Tr)-ke*(Tr-8))
     return [dTzco,dTpm,dTpco,dTr]
 
 def sim(y0,value,Tzm,Tpco,time):
     #params Mm Mco Cw ro cw kw Fzco Fzm Tzm Tpco value
     y0=np.array(y0)
-    params=[3000,3000,2700,1000,4200,250000,41.6,22.2,Tzm,Tpco,value]
+    params=[3000,3000,2700,1000,4200,250000,0.023,0.042,Tzm,Tpco,value]
     t=np.arange(0., 1, 0.1)
     s=ode(wymiennik, y0, t,args=(params,))
     w=(s[-1]-y0)*time+y0
@@ -73,7 +73,7 @@ def sim2(y0,value,Tzm,Tpco,Tr,time):
     #params Mm Mco Cw ro cw kw Fzco Fzm Tzm Tpco value
     y0=[y0[0],y0[1],Tpco,Tr]
     y0=np.array(y0)
-    params=[3000,3000,2700,1000,4200,250000,41.6,22.2,Tzm,Tpco,value]
+    params=[3000,3000,2700,1000,4200,250000,0.023,0.042,Tzm,Tpco,value]
     t=np.arange(0., time, 0.1)
     s=ode(wymiennikz, y0, t,args=(params,))
     return s[-1]
